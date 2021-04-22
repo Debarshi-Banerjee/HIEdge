4d0904ff6e554e2d1355feacc354f8b127c8646c
Debarshi Banerjee <debarshi.b.93@gmail.com>
2021-04-22 22:44:09 +0530

    Small changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 
 const sampleData = {
   gambits: {
+    
+    // H
     1: {
-      name: "FirstGambit",
+      name: "1",
       position: { x: 300, y: 200 },
       branches: [
         {
@@ -20,23 +22,18 @@ const sampleData = {
       ]
     },
     2: {
-      name: "SecondGambit",
+      name: "2",
       position: { x: 100, y: 100 },
       branches: [
 				{
-          branch_id: "b_3",
-          connect_gid: "g_4",
-          output: "o_3"
-        },
-        {
           branch_id: "b_5",
           connect_gid: "g_6",
-          output: "o_5"
+          output: "o_6"
         }
 			]
     },
     3: {
-      name: "ThirdGambit",
+      name: "3",
       position: { x: 500, y: 100 },
       branches: [{
 				branch_id: "b_4",
@@ -45,21 +42,51 @@ const sampleData = {
 			}]
 		},
 		4: {
-      name: "ForthGambit",
-      position: { x: 100, y: 300 },
+      name: "4",
+      position: { x: 300, y: 400 },
       branches: []
 		},
 		5: {
-      name: "FifthGambit",
-      position: { x: 500, y: 300 },
-      branches: []
+      name: "5",
+      position: { x: 495, y: 500 },
+      branches: [{
+				branch_id: "b_9",
+				connect_gid: "g_4",
+				output: "o_3"
+			}]
 		},
 		6: {
-      name: "SixthGambit",
-      position: { x: 300, y: 500 },
-      branches: []
-    }
+      name: "6",
+      position: { x: 94, y: 500 },
+      branches: [
+				{
+          branch_id: "b_3",
+          connect_gid: "g_4",
+          output: "o_3"
+        }
+			]
+    },
+
+
+
+
+
+
+// I
+    7:{
+      name:"7",
+      position:{x:700,y:300},
+      branches:[]
+    },
+    8:{
+      name:"8",
+      position:{x:800,y:300},
+      branches:[]
+    },
   }
+
+
+
 };
 
 function App() {
